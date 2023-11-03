02e6376c98f3a4b7bba5b0dae6c2108d1fd344aa
Kimgmltn <Kimgmltn0323@gmail.com>
2023-11-04 00:35:42 +0900

    companyRepository 생성 및 테스트

---
+++ src/test/java/com/smartorder/repository/CompanyRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.smartorder.repository;
+
+import com.smartorder.entity.Company;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@DataJpaTest
+//@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+class CompanyRepositoryTest {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Test
+    @DisplayName("회사 save")
+    void save(){
+        Company company = Company.builder()
+                .companyName("갈비대첩")
+                .build();
+        Company save = companyRepository.save(company);
+        Company find  = companyRepository.findByCompanyName("갈비대첩").get();
+        Assertions.assertThat(find.getCompanyName()).isEqualTo(save.getCompanyName());
+    }
+
+    @Test
+    @DisplayName("회사명 변경")
+    void update_save(){
+        Company company = Company.builder()
+                .companyName("갈비대첩")
+                .build();
+        companyRepository.save(company);
+
+        Company find  = companyRepository.findByCompanyName("갈비대첩").get();
+        find.setCompanyName("갈비대첩2");
+        companyRepository.save(find);
+
+        Company find2 = companyRepository.findByCompanyName("갈비대첩2").get();
+        Assertions.assertThat(find.getCompanyId()).isEqualTo(find2.getCompanyId());
+    }
+
+}
