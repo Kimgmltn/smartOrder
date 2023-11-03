a609097eff89953d36c6c03b6d34f4bf07efd23b
Kimgmltn <Kimgmltn0323@gmail.com>
2023-11-04 01:25:37 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/smartorder/SmartorderApplication.java
+++ src/main/java/com/smartorder/SmartorderApplication.java
@@ -2,8 +2,10 @@ package com.smartorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SmartorderApplication {
 
     public static void main(String[] args) {
